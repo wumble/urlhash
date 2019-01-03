5642c7210c3d6486afcf1a0688b954cfcf7baa40
Manuel Bartels <mail@manuelbartels.de>
2019-01-03 20:00:06 +0100

    var to const & let

--- urlHash.js
+++ urlHash.js
@@ -1,16 +1,16 @@
 function urlHash() {
 
   function getUrlHashParameter(param) {
-    var params = getUrlHashParameters();
+    const params = getUrlHashParameters();
 
     return params[param];
   }
 
   function getUrlHashParameters() {
-    var sPageURL = window.location.hash;
+    let sPageURL = window.location.hash;
     if (sPageURL) sPageURL = sPageURL.split('#')[1];
-    var pairs = sPageURL.split('&');
-    var object = {};
+    const pairs = sPageURL.split('&');
+    const object = {};
     pairs.forEach(function(pair, i) {
       pair = pair.split('=');
       if (pair[0] != '') object[pair[0]] = pair[1];
@@ -22,23 +22,25 @@ function urlHash() {
   // accepts an object like { paramName: value, paramName1: value }
   // and transforms to: url.com#paramName=value&paramName1=value
   function setUrlHashParameters(params) {
-    var keys = Object.keys(params);
-    var values = Object.values(params);
-    var pairs = [];
+    const keys = Object.keys(params);
+    const values = Object.values(params);
+    const pairs = [];
     keys.forEach(function(key, i) {
       if (key != '') pairs.push(keys[i] + '=' + values[i]);
     });
-    var hash = pairs.join('&');
+    const hash = pairs.join('&');
+
     window.location.hash = hash;
   }
 
   function setUrlHashParameter(param, value) {
-    var params = getUrlHashParameters();
+    const params = getUrlHashParameters();
     if (value === null) {
       delete params[param];
     } else {
       params[param] = value;
     }
+
     setUrlHashParameters(params);
   }
 
